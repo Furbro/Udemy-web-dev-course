43e9d4461fbe5e1a344d17f1bb2bcfabb0dcd78a
Furbro Laptop <mofr2612@gmail.com>
2025-05-27 19:16:05 +0200

    changes to folder, node modules ect.

--- Node/2.2 Native Modules/index.js
+++ Node/2.2 Native Modules/index.js
@@ -1,1 +1,7 @@
-console.log("I am a POTATO!");
+const fs = require("fs");
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+
+  console.log(data);
+});
---
+++ Node/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// Fritjof sitt forsøk.
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}`);
+
+// import superheroes, { randomSuperhero } from "superheroes";
+// var superhero = randomSuperhero();
+// console.log(`BUT, my superhero name is ${superhero}`);
+
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}`);
+
+import superheroes, { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
